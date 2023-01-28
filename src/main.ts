c87124c39b53e7ed09166dbcdb9a3a1d95c269f6
thebigG <yunior.eury@gmail.com>
2023-01-28 17:50:23 -0600

    -Add robot.

--- src/main.ts
+++ src/main.ts
@@ -167,6 +167,58 @@ function dinosaurs() {
   return sketch;
 }
 
+function robot() {
+  var sketch = (p5: p5) => {
+    // The sketch setup method
+    p5.setup = () => {
+      // Creating and positioning the canvas
+      const canvas = p5.createCanvas(720, 480);
+      p5.strokeWeight(2);
+      p5.ellipseMode(p5.RADIUS);
+    };
+
+    // The sketch draw method
+    p5.draw = () => {
+      // DEMO: Let the circle instances draw themselves
+      p5.background(204);
+
+      // Neck
+      p5.stroke(102);
+      p5.line(266, 257, 266, 162); // Left
+      p5.line(276, 257, 276, 162); // Middle
+      p5.line(286, 257, 286, 162); // Right
+
+      // Antennae
+      p5.line(276, 155, 246, 112); // Small
+      p5.line(276, 155, 306, 56); // Tall
+      p5.line(276, 155, 342, 170); // Tall
+
+      // Body
+      p5.noStroke();
+      p5.fill(102);
+      p5.ellipse(264, 377, 33, 33); // Antigravity Orb
+      p5.fill(0);
+      p5.rect(219, 257, 90, 120);
+      p5.fill(102);
+      p5.rect(219, 274, 90, 6);
+
+      // Head
+      p5.fill(0);
+      p5.ellipse(276, 155, 45, 45);
+      p5.fill(255);
+      p5.ellipse(288, 150, 14, 14);
+      p5.fill(0);
+      p5.ellipse(288, 150, 3, 3);
+      p5.fill(153);
+      p5.ellipse(263, 148, 5, 5);
+      p5.ellipse(296, 130, 4, 4);
+      p5.ellipse(305, 162, 3, 3);
+    };
+  };
+
+  return sketch;
+}
+
 function main(): void {
   // new p5(circle());
   // new p5(circleAndMouse());
@@ -174,7 +226,7 @@ function main(): void {
   // new p5(triangle());
   // new p5(arcs());
   // new p5(arrow());
-  new p5(dinosaurs());
+  new p5(robot());
 }
 
 main();
