2d5405e1b91f6d4fa70be5e0b728c76027fbeebf
thebigG <yunior.eury@gmail.com>
2023-01-30 22:24:06 -0600

    -Add Vector2. WIP.

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,6 +1,13 @@
 import { expect, it } from "@jest/globals";
-import { distance } from "../../src/util";
+import { distance, Vector2 } from "../../src/util";
 
 it("distance function", () => {
   expect(distance(0, 0, 0, 10)).toBe(10);
+});
+
+it("vector test", () => {
+  let v1 = new Vector2(10, 5);
+  expect(v1.getX()).toBe(10);
+  expect(v1.getY()).toBe(5);
+  expect(v1.getLength().toFixed(2)).toBe("11.18");
 });
--- src/util.ts
+++ src/util.ts
@@ -9,3 +9,43 @@ export function distance(
 
   return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 }
+
+export class Vector2 {
+  private x: number;
+  private y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public getX(): number {
+    return this.x;
+  }
+  public setX(value: number) {
+    this.x = value;
+  }
+
+  public getY(): number {
+    return this.y;
+  }
+  public setY(value: number) {
+    this.y = value;
+  }
+
+  public setAngle(angle: number) {
+    let length = this.getLength();
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
+  }
+
+  public getAngle() {
+    Math.atan2(this.y, this.x);
+  }
+
+  public getLength(): number {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+
+  public add() {}
+}
